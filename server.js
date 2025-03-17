2fd054d243fe73a5cbbb984bba8f9fc2b22d2b5a
SELVAKUMAR2003794 <selva91823@gmail.com>
2025-03-17 20:36:25 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const attendanceRoutes = require('./routes/attendanceRoutes'); // Ensure this path is correct
+
+const app = express();
+const PORT = 5500;
+const MONGO_URL = `mongodb+srv://selva91823:Selva91823@cluster0.sxbcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const MONGO_URL = `mongodb+srv://selva91823:Selva91823@cluster0.sxbcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URL, {
+
+})
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+
+app.use('https://studentattendance-5jge.onrender.com/api/students', attendanceRoutes); // Ensure this path is correct
+setTimeout(() => {
+    app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
+
+}, 3000)
+
